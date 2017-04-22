2ce9cbf987e4e0bd8a67ed60638da7e4229a81fe
Dave Brookshire <dsb@hpe.com>
2017-04-22 08:21:43 -0500

    Tweaking sample application

--- sample.py
+++ sample.py
@@ -4,12 +4,23 @@
 #  Author: Dave Brookshire <dsb@hpe.com>
 #
 #
+import time
 
 from rhusb.sensor import RHUSB
 
+delay = 1
+count = 10
+
 if __name__ == '__main__':
     sens = RHUSB()
-    print("[{0}]".format(sens.PA()))
-    print("[{0}]".format(sens.C()))
-    print("[{0}]".format(sens.F()))
-    print("[{0}]".format(sens.H()))
+    print("PA: [{0}]".format(sens.PA()))
+    print("C: [{0}]".format(sens.C()))
+    print("F: [{0}]".format(sens.F()))
+    print("H: [{0}]".format(sens.H()))
+
+    print("\nStarting periodic readings every {0} seconds".format(delay))
+
+    while count:
+        print("--> {0}".format(sens.PA()))
+        count -= 1
+        time.sleep(delay)
