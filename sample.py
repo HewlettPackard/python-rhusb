ba0048fd01a1de985edd6d69567886ef0ea77c72
Dave Brookshire <dsb@hpe.com>
2017-05-24 12:25:46 -0500

    Add exception handling to the sample script.

--- sample.py
+++ sample.py
@@ -5,6 +5,7 @@
 #
 #
 import time
+import serial
 
 from rhusb.sensor import RHUSB
 
@@ -12,15 +13,19 @@ delay = 1
 count = 10
 
 if __name__ == '__main__':
-    sens = RHUSB()
-    print("PA: [{0}]".format(sens.PA()))
-    print("C: [{0}]".format(sens.C()))
-    print("F: [{0}]".format(sens.F()))
-    print("H: [{0}]".format(sens.H()))
+    try:
+        sens = RHUSB()
+        print("PA: [{0}]".format(sens.PA()))
+        print("C: [{0}]".format(sens.C()))
+        print("F: [{0}]".format(sens.F()))
+        print("H: [{0}]".format(sens.H()))
 
-    print("\nStarting periodic readings every {0} seconds".format(delay))
+        print("\nStarting periodic readings every {0} seconds".format(delay))
 
-    while count:
-        print("--> {0}".format(sens.PA()))
-        count -= 1
-        time.sleep(delay)
+        while count:
+            print("--> {0}".format(sens.PA()))
+            count -= 1
+            time.sleep(delay)
+
+    except serial.serialutil.SerialException:
+        print("Error: Unable to open RH-USB Serial device.")
